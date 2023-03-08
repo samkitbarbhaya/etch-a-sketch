7503c2dfb02f7ee65e6a81233729ee41db188b44
Samkit Barbhaya <samkitbarbhaya@gmail.com>
2023-03-07 20:04:00 -0700

    Adding new styles

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var slider = document.querySelector('#myRange');
-var output = document.querySelector("#demo");
-output.innerHTML = `${slider.value}x${slider.value}`; // Display the default slider value
+var slider = document.querySelector('#size-slider');
+var output = document.querySelector("#size-value");
+output.innerHTML = `${slider.value} x ${slider.value}`; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = `${this.value}x${this.value}`;
+  output.innerHTML = `${this.value} x ${this.value}`;
 }
