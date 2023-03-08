c9cddcb9b6095b34616be7fc709d70104a0e690e
Samkit Barbhaya <samkitbarbhaya@gmail.com>
2023-03-08 15:37:01 -0700

    Added variables in JS File

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-var slider = document.querySelector('#size-slider');
-var output = document.querySelector("#size-value");
-output.innerHTML = `${slider.value} x ${slider.value}`; // Display the default slider value
+const DEFAULT_VALUE = 16;
+const slider = document.querySelector('#size-slider');
+const gridSizeValueDiv = document.querySelector("#size-value");
+const grid = document.getElementById('grid')
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = `${this.value} x ${this.value}`;
+    gridSizeValueDiv.innerHTML = `${this.value} x ${this.value}`;
+}
+
+function setupGrid(size) {
+
+}
+
+window.onload = () =>{
+    // setupGrid(slider.value)
+    gridSizeValueDiv.innerHTML = `${DEFAULT_VALUE} x ${DEFAULT_VALUE}`; // Display the default slider value
 }
